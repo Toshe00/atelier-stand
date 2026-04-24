f0e29c8202eb626b25fd98da3dba549d78bc4b8a
Toshe00 <rpsbatllepro@gmail.com>
2026-04-24 22:12:33 +0200

    modification du site

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -5,59 +5,59 @@ const heroLinks = [
   {
     href: "/",
     label: "Accueil Atelier Stand",
-    className: "left-[3.6%] top-[2.4%] h-[11%] w-[18.8%]",
+    className: "left-[3.8%] top-[2.4%] h-[9.2%] w-[18.6%]",
   },
   {
     href: "/configurator",
     label: "Configurateur",
-    className: "left-[31.7%] top-[3.3%] h-[7.2%] w-[10.4%]",
+    className: "left-[32.4%] top-[3.3%] h-[7.5%] w-[9.6%]",
   },
   {
     href: "/shop",
     label: "Boutique",
-    className: "left-[43.1%] top-[3.3%] h-[7.2%] w-[6.6%]",
+    className: "left-[42.9%] top-[3.3%] h-[7.5%] w-[6.5%]",
   },
   {
     href: "/gallery",
     label: "Réalisations",
-    className: "left-[50.1%] top-[3.3%] h-[7.2%] w-[9.2%]",
+    className: "left-[50%] top-[3.3%] h-[7.5%] w-[9.2%]",
   },
   {
     href: "/about",
     label: "À propos",
-    className: "left-[59.6%] top-[3.3%] h-[7.2%] w-[7%]",
+    className: "left-[59.2%] top-[3.3%] h-[7.5%] w-[7.2%]",
   },
   {
     href: "/contact",
     label: "Contact",
-    className: "left-[66.7%] top-[3.3%] h-[7.2%] w-[7.7%]",
+    className: "left-[66.2%] top-[3.3%] h-[7.5%] w-[7.8%]",
   },
   {
     href: "/cart",
     label: "Panier",
-    className: "left-[79.1%] top-[2.6%] h-[7.7%] w-[4.4%]",
+    className: "left-[79.1%] top-[2.7%] h-[8.2%] w-[3.4%]",
   },
   {
     href: "/configurator",
     label: "Créer mon stand",
-    className: "left-[82.4%] top-[2.3%] h-[8.1%] w-[13.2%]",
+    className: "left-[82.2%] top-[2.5%] h-[8.2%] w-[13.2%]",
   },
   {
     href: "/configurator",
     label: "Créer votre stand",
-    className: "left-[5.6%] top-[70.6%] h-[7.1%] w-[16.3%]",
+    className: "left-[5.4%] top-[69.2%] h-[8.2%] w-[16.6%]",
   },
   {
     href: "/shop",
     label: "Voir les modèles",
-    className: "left-[23.3%] top-[70.6%] h-[7.1%] w-[13.3%]",
+    className: "left-[23%] top-[69.2%] h-[8.2%] w-[13.9%]",
   },
 ];
 
 export function Hero() {
   return (
     <section className="bg-[#080705]">
-      <div className="relative aspect-[1536/762] w-full overflow-hidden">
+      <div className="relative aspect-[1780/883] w-full overflow-hidden">
         <Image
           src="/images/home-pages.png"
           alt="Atelier Stand - des stands en bois sur mesure pour sublimer votre marque"
